6e892b6cdd33b41124bceb81d65924cbadf1c650
Isaac <silong@mtu.edu>
2019-04-21 17:20:40 -0400

    added error message

--- mainsite/accountant/pipeline.py
+++ mainsite/accountant/pipeline.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 
-def verify_scope(backend, user, response, *args, **kwargs):
+def verify_scope(request, backend, user, response, *args, **kwargs):
     if not user.email.endswith('@mtu.edu'):
+        messages.warning(request, 'Please log in with your MTU email.')
         return HttpResponseRedirect('/')
