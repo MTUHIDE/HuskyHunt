6eedb1717d52226f7eec3efb5a07896c6e3368b8
SamV-42 <sjvander@mtu.edu>
2019-10-29 20:01:28 -0400

    Another intermediate save
    Turns out this nifty thing called forms exist

--- mainsite/accountant/views.py
+++ mainsite/accountant/views.py
@@ -7,24 +7,71 @@ from .models import Account
 from django.contrib import auth
 from catalog.models import CatalogItem, Category, SubCategory
 from django.utils import timezone
+from django.urls import reverse
 
 from django.views.generic import DetailView
 from django.contrib.auth.models import User
+from django.core.files.storage import FileSystemStorage
 
-class AccountDetailView(DetailView):
-    model = Account
-    context_object_name = "account"
+from django.forms import ModelForm
 
-    def get_object(self, queryset=None):
-        try:
-            return Account.objects.get(user = self.request.user)
-        except Account.DoesNotExist:
-            createDefaultAccount(request.user)
-            return Account.objects.get(user = self.request.user)
 
-def createDefaultAccount(user):
-    acct = Account(user = user)
-    acct.save()
+class EditModelForm(ModelForm):
+    class Meta:
+        model = Account
+        fields = ['bio', 'street_address', 'city', 'zipcode', 'common_destination_zipcode', 'rating']
+
+def edit(request):
+    try:
+        currentUser = Account.objects.get(user = request.user)
+    except Account.DoesNotExist:
+        createDefaultAccount(request.user)
+        currentUser = Account.objects.get(user = request.user)
+
+    if request.method == "POST":
+        form = EditModelForm(request.POST, instance = currentUser)
+        form.save()
+
+        return HttpResponseRedirect(reverse('accountant:index'))
+
+    #else # request.method == "GET"
+    form = EditModelForm(instance = currentUser)
+    return render(request, 'accountant/account_detail.html', {'form': form})
+
+# class AccountDetailView(DetailView):
+#     model = Account
+#     context_object_name = "account"
+#
+#     def get_object(self, queryset=None):
+#         try:
+#             return Account.objects.get(user = self.request.user)
+#         except Account.DoesNotExist:
+#             createDefaultAccount(request.user)
+#             return Account.objects.get(user = self.request.user)
+#
+# def createDefaultAccount(user):
+#     acct = Account(user = user)
+#     acct.save()
+#
+# def setEdit(request):
+#     userAccount = Account.objects.get(user = request.user)
+#
+#     if 'bio' in request.POST:
+#         userAccount.bio = request.POST['bio']
+#     if 'street' in request.POST:
+#         userAccount.street_address = request.POST['street']
+#     if 'city' in request.POST:
+#         userAccount.city = request.POST['city']
+#     if 'zip' in request.POST:
+#         userAccount.zipcode = request.POST['zip']
+#     if 'cdzip' in request.POST:
+#         userAccount.common_destination_zipcode = request.POST['cdzip']
+#     if 'pic_upload' in request.FILES:
+#         uploadedFile = request.FILES['pic_upload']
+#
+#    userAccount.save()
+#
+#    return HttpResponseRedirect(reverse('accountant:index'))
 
 
 def catalogRedirect(request):
