a8c0809c518ec96605dec01176be04f26e6a3a39
SamV-42 <sjvander@mtu.edu>
2019-11-03 02:41:31 -0500

    Complete Edit Account Page
    Minor Javascript, and *significant* fiddling to makea custom Widget class, was required to preview the uploaded image. Hopefully, though, this tech will be reusable in other upload-image places on the site.

--- mainsite/accountant/views.py
+++ mainsite/accountant/views.py
@@ -15,11 +15,73 @@ from django.core.files.storage import FileSystemStorage
 
 from django.forms import ModelForm
 
+from django.forms.widgets import ClearableFileInput, TextInput
+from django.template import loader
+from django.utils.safestring import mark_safe
+
+from django.utils.translation import gettext_lazy as _
+
+class PreviewImageWidget(ClearableFileInput):
+    template_name = "accountant/preview_image.html"
+
+    clear_button_label = _('Reset')
+    default_width = 150
+    default_height = 150
+    defaultPictureURL = 'https://www.mtu.edu/mtu_resources/images/download-central/social-media/gold-name.jpg'
+
+    def clear_button_name(self, name):
+        return name + '-clear'
+    def clear_button_id(self, name):
+        return name + '_id'
+    def preview_image_name(self, name):
+        return name + '-preview'
+    def preview_image_id(self, name):
+        return name + '_id'
+    #def format_value:
+
+    def get_context(self, name, value, attrs):
+        width = attrs.get('width', self.default_width)
+        height = attrs.get('height', self.default_height)
+
+        button_name = self.clear_button_name(name)
+        button_id = self.clear_button_id(button_name)
+        preview_name = self.preview_image_name(name)
+        preview_id = self.preview_image_id(preview_name)
+
+        context = super().get_context(name, value, attrs)
+        some_unneeded_fields = ['checkbox_name', 'checkbox_id', 'clear_button_label', 'is_initial', 'initial_text']
+        for i in some_unneeded_fields:
+            context.pop(i, None)
+
+        context['widget'].update({
+            #'value':  #presumably passed up in the ModelForm
+            'defaultPictureURL': self.defaultPictureURL,
+
+            'button_name': button_name,
+            'button_id': button_id,
+            'clear_button_label': self.clear_button_label,
+
+            #name, attrs -- inherited
+            'id': (name + '_id'),
+
+            'preview_text': "Uploaded Image",
+            'preview_id': preview_id,
+            'preview_width': width,
+            'preview_height': height,
+        })
+
+        return context
+
+    def value_from_datadict(self, data, files, name):
+        return files.get(name)
 
 class EditModelForm(ModelForm):
     class Meta:
         model = Account
         fields = ['bio', 'street_address', 'city', 'zipcode', 'common_destination_zipcode', 'picture']
+        widgets = {
+            'picture': PreviewImageWidget()
+        }
 
 def edit(request):
     try:
@@ -29,12 +91,15 @@ def edit(request):
         currentUser = Account.objects.get(user = request.user)
 
     if request.method == "POST":
+        if request.FILES.get('picture') is None:
+            currentUser.picture = None
         form = EditModelForm(request.POST, request.FILES, instance = currentUser)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('accountant:index'))
     else: # request.method == "GET"
         form = EditModelForm(instance = currentUser)
+
     return render(request, 'accountant/account_detail.html', {'form': form})
 
 
@@ -47,8 +112,14 @@ def logout(request):
 
 def index(request):
    if request.user.is_authenticated:
+      try:
+          currentUser = Account.objects.get(user = request.user)
+      except Account.DoesNotExist:
+          createDefaultAccount(request.user)
+          currentUser = Account.objects.get(user = request.user)
+
       template = 'accountant/index.html'
-      defaultPicture= 'https://www.mtu.edu/mtu_resources/images/download-central/social-media/gold-name.jpg'
+      defaultPicture = 'https://www.mtu.edu/mtu_resources/images/download-central/social-media/gold-name.jpg'
       my_items = CatalogItem.objects.filter(username = request.user)
       filters = Category.objects.all()
       title = 'My items'
@@ -56,7 +127,7 @@ def index(request):
                   'item_list':my_items,
                   'title': title,
                   'filters': filters,
-                  'defaultPicture': defaultPicture,
+                  'defaultPicture': currentUser.picture.url if currentUser.picture else defaultPicture,
         }
       return render(request, template, context)
    else:
