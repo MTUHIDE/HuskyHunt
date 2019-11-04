79f50a7a76cba21f66930181fa4d3952d562e39f
cjvidro <cjvidro@mtu.edu>
2019-11-04 10:58:56 -0500

    Commenting, database item filling

--- mainsite/accountant/views.py
+++ mainsite/accountant/views.py
@@ -1,9 +1,5 @@
-#from django.views.generic import ListView, CreateView, UpdateView
-#from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-#from .forms import AccountForm
-#from .models import Account
 from django.contrib import auth
 from catalog.models import CatalogItem, Category, SubCategory
 from django.utils import timezone
@@ -12,39 +8,28 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 
 # Create your views here.
-#class AccountListView(ListView):
-#    model = Account
-#    contex_object_name = 'obj'
-#
-#class AccountCreateView(CreateView):
-#    model = Account
-#    fields = ('item_title', 'item_description', 'item_price', 'category')
-#    success_url = reverse_lazy('index')
-#
-#class AccountUpdateView(UpdateView):
-#    model = Account
-#    fields = ('item_title', 'item_description', 'item_price', 'category')
-#    success_url = reverse_lazy('index')
 
-
+# Redirect to main catalog page
 def catalogRedirect(request):
     return HttpResponseRedirect('/catalog')
 
+# Logout link
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect('/')
 
+# Loads the primary account page
 def index(request):
-   if request.user.is_authenticated:
+  # Check if user is authenticated
+  if request.user.is_authenticated:
+      # Gather all of the resources the template uses
       template = 'accountant/index.html'
       defaultPicture= 'https://www.mtu.edu/mtu_resources/images/download-central/social-media/gold-name.jpg'
       my_items = CatalogItem.objects.filter(username = request.user)
       filters = Category.objects.all()
       title = 'My items'
 
-      u = User.objects.get(username=request.user)
-      preferred_name = u.user_profile.preferred_name
-
+      # Put data in context to be accessed from template
       context = {
                   'item_list':my_items,
                   'title': title,
@@ -52,9 +37,10 @@ def index(request):
                   'defaultPicture': defaultPicture
         }
       return render(request, template, context)
-   else:
+  else:
       return HttpResponseRedirect('/')
 
+# Used as an intermediate function to delete an item
 def deleteItem(request, pk):
   if request.user.is_authenticated:
     # delete item from database
