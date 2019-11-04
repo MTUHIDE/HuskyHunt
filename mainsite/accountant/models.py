d61164f688dd3d66a0cd5a94bd7a4986ddd72468
cjvidro <cjvidro@mtu.edu>
2019-11-04 11:11:09 -0500

    Deleted old model code

--- mainsite/accountant/models.py
+++ mainsite/accountant/models.py
@@ -9,31 +9,6 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Create your models here.
 
-# class Account(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     bio = models.TextField(blank=True)
-#     street_address = models.CharField(max_length=200, blank=True)
-#     city = models.CharField(max_length=100, blank=True)
-#     zipcode = models.IntegerField(default=0, blank=True)
-#     common_destination_zipcode = models.IntegerField(default=0, blank=True)
-#     picture = models.ImageField(upload_to='account/profilepics/', height_field=None, width_field=None, blank=True, null=True)
-#     rating = models.IntegerField(default=0)
-
-#     USERNAME_FIELD = 'user'
-#     REQUIRED_FIELDS = []
-
-#     def __str__(self):
-#         return self.user.username
-
-#     def is_anonymous(self):
-#         return False
-
-#     def get_short_name(self):
-#         return self.user.username
-
-#     def is_authenticated(self):
-#         return True
-
 class user_profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     preferred_name = models.CharField(max_length=50, blank = True, null=True)
