8b2cb37a0b1afd555f64abd2865d612bc9d63a8c
SamV-42 <sjvande@mtu.edu>
2019-11-12 18:37:22 -0500

    Basic ridesharing form
    Got to ridesharing/create to enter it.

--- mainsite/rideSharing/urls.py
+++ mainsite/rideSharing/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
 
     #...blank then execute the index methon in views
     path('', views.index, name='index'),
+    path('create', views.RideCreate.as_view(), name='ride_create')
 ]
