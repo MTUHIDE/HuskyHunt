3ad3e8b0cc4a7545986e0a197e23ba2f3846f346
SamV-42 <sjvander@mtu.edu>
2019-11-17 00:48:40 -0500

    Display preferred name in catalog
    Finds associated user_profile and uses preferred_name or, if not present, first name. Legacy catalog items exist that use a username rather than an actual user.

--- mainsite/catalog/models.py
+++ mainsite/catalog/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User # imports the user table
 from django.db import models
 from django.conf import settings
+from accountant.models import user_profile
+
 
 #Defines a table of categories
 class Category(models.Model):
@@ -22,10 +24,10 @@ class Category(models.Model):
 class SubCategory(models.Model):
     #Each subcategory has a name, parent category, date of creation and the last update
 
-    #The subcategory name can be up to 200 letters in length 
+    #The subcategory name can be up to 200 letters in length
     category_name = models.CharField(max_length=200)
 
-    #The subcategory is tied to one of the main categories 
+    #The subcategory is tied to one of the main categories
     #and is deleted if the parent is deleted
     subcategory_of = models.ForeignKey(Category, on_delete=models.CASCADE)
 
@@ -42,14 +44,24 @@ class SubCategory(models.Model):
 class CatalogItem(models.Model):
     #Each item has a username, category, subcategory, date added, title, description
     #picture, and price associated to it
-    
+
     #The username is automatically set to the user that added the item
     #and the item is deleted if the user is deleted
     username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    
-    # The user's first (preferred) name
-    first_name = models.CharField(max_length=25)
-    
+
+    #The preferred name of the item in question;
+    # note, I'm unsure of the performance implications of making this query all the time
+    def get_preferred_first_name(self):
+        try:
+            user = user_profile.objects.get(user=self.username)
+        except user_profile.DoesNotExist:
+            return self.username
+        name = user.preferred_name
+        if not name or name == "":
+            name = self.username.first_name
+        return name
+    first_name = property(get_preferred_first_name)
+
     #The category is set by the user and the item is deleted if its category is deleted
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
@@ -67,10 +79,10 @@ class CatalogItem(models.Model):
 
     #Picture that is uploaded on item creation
     item_picture = models.ImageField(upload_to='catalog/%Y/%m/%d/', height_field=None, width_field=None)
-    
+
     #The price that the user wants to sell the item at
     item_price = models.DecimalField(max_digits=7, decimal_places=2, default=0.00)
-    
+
     #An integer identifying the item
     views = models.IntegerField(default=0)
 
