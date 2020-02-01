e86fed23733631fe459ccf94ac012cfd5b597497
cjvidro <cjvidro@mtu.edu>
2020-02-01 15:51:57 -0500

    Added ride searching functionality

--- mainsite/rideSharing/views.py
+++ mainsite/rideSharing/views.py
@@ -44,3 +44,73 @@ def index(request):
         return render(request, template, context)
     else:
         return HttpResponseRedirect('/')
+
+
+#This small helper function adds an appropriate error message to the page
+#param: context - the context that's normally passed to the ridesharing pages;
+#         it's modified appropriately during this function to contain recent rides
+#param: type - one of 'SearchFail', 'FilterFail', 'PageNotFoundFail', etc.
+#param: num_items - The number of recent rides displayed, default is 4 most recent
+#returns: boolean whether or not
+def addErrorOnEmpty(context, type, num_items = 4):
+    context['failed_search'] = None
+    if context['rides'].paginator.count == 0:
+
+        #Gets num_items most recent items from the database and sorts by date added
+        recent_items = RideItem.objects.filter(
+            date_added__lte=timezone.now()
+        ).order_by('-date_added')[:num_items]
+
+        # Paginator will show up to num_items items. Always one page long.
+        paginator = Paginator(recent_items, num_items, allow_empty_first_page=True)
+        rides = paginator.get_page(1)
+
+
+        context['failed_search'] = type
+        context['rides'] = rides
+        return True
+    return False
+
+#This function takes information from the search textfield
+#param: request - array variable that is passed around the website, kinda like global variables
+#returns: all items in the database that contain the string
+#from the search text field in their name or description
+def search(request):
+    #The CSS code for this function can be found here
+    template = 'rideSharing/index.html'
+
+    #The title for the webpage
+    title = 'MTU Ridesharing'
+
+    #Checks to make sure the user has logged in
+    if request.user.is_authenticated:
+        #Uses the filter function to get the data of the searched items
+        recent_items = RideItem.objects.filter(
+            Q(start_city__contains=request.GET['search']) | 
+            Q(start_state__contains=request.GET['search']) |
+            Q(start_zipcode__contains=request.GET['search']) |
+            Q(destination_city__contains=request.GET['search']) |
+            Q(destination_state__contains=request.GET['search']) |
+            Q(destination_zipcode__contains=request.GET['search']) |
+            Q(notes__contains=request.GET['search'])
+        )[:200]
+
+        # Paginator will show 16 items per page
+        paginator = Paginator(recent_items, 16, allow_empty_first_page=True)
+        page = request.GET.get('page') # Gets the page number to display
+        rides = paginator.get_page(page)
+
+        #Puts all the data to be displayed into context
+        context = {
+          'rides': rides,
+          'title': title,
+        }
+
+        addErrorOnEmpty(context, 'SearchFail')
+
+        #Returns a render function call to display onto the website for the user to see
+        return render(request, template, context)
+
+    #If the user is not logged in then they get redirected to the HuskyStatue screen
+    else:
+        return HttpResponseRedirect('/')
