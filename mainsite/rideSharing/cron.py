302f7fed193fb25b81e50d66764604767a13a0c3
cjvidro <cjvidro@mtu.edu>
2020-03-01 13:41:43 -0500

    Ride archive cron job

--- mainsite/rideSharing/cron.py
+++ mainsite/rideSharing/cron.py
@@ -1,13 +1,25 @@
 from django_cron import CronJobBase, Schedule
 from rideSharing.models import RideItem
+from datetime import date
 
 
 # Used to archive rides that have already "left"
-class MyCronJob(CronJobBase):
-    RUN_EVERY_MINS = 120 # every 2 hours
+class ArchiveRides(CronJobBase):
+    RUN_AT_TIMES = ['04:00'] # Run at 4am
 
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = 'rideSharing.my_cron_job'    # a unique code
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
+    code = 'rideSharing.archive_rides'    # a unique code
 
     def do(self):
-        pass    # do your thing here
+        today = date.today()
+
+        # Get the rides that left before today
+        old_rides = RideItem.objects.filter(
+            archived='False',
+            date_leaving__lte=today
+        )
+
+        # For each ride, set it to archived
+        for ride in old_rides:
+        	ride.archived = "True"
+        	ride.save()
