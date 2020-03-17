c97af0314951624765ac79f11ee0b91a48c38bdb
cjvidro <cjvidro@mtu.edu>
2020-03-17 18:53:23 -0400

    Added deleteOldRides cron

--- mainsite/rideSharing/cron.py
+++ mainsite/rideSharing/cron.py
@@ -1,6 +1,7 @@
 from django_cron import CronJobBase, Schedule
 from rideSharing.models import RideItem
 from datetime import date
+from django.utils import timezone
 
 
 # Used to archive rides that have already "left"
@@ -13,8 +14,8 @@ class ArchiveRides(CronJobBase):
     def do(self):
         today = date.today()
 
-        # Get the rides that left before today
-        old_rides = RideItem.objects.filter(
+        # Considered archived after the ride has left
+        archive_rides = RideItem.objects.filter(
             archived='False',
             date_leaving__lte=today
         )
@@ -23,3 +24,23 @@ class ArchiveRides(CronJobBase):
         for ride in old_rides:
         	ride.archived = "True"
         	ride.save()
+
+# Used to delete old archived rides
+class deleteOldRides(CronJobBase):
+    RUN_AT_TIMES = ['04:00'] # Run at 4am
+
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
+    code = 'catalog.deleteOldRides'    # a unique code
+
+    def do(self):
+        today = timezone.now()
+        delete_time = today - timezone.timedelta(weeks=16) # Considered to be deleted if the ride left more than 16 weeks ago
+
+        # Get the rides that left before today
+        delete_rides = RideItem.objects.filter(
+            archived='True',
+            date_leaving__lte=delete_time
+        )
+
+        # Delete these old rides
+        delete_rides.delete()
