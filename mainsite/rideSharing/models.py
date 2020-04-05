07e4341cedae3a5d6874686d872ad0e2851e1441
cjvidro <cjvidro@mtu.edu>
2020-04-05 14:03:37 -0400

    Not all rides start in Houghton

--- mainsite/rideSharing/models.py
+++ mainsite/rideSharing/models.py
@@ -71,6 +71,10 @@ class RideItem(models.Model):
     destination_coordinates_lat = models.DecimalField(decimal_places=6, max_digits=14, default=0.00)
     destination_coordinates_lon = models.DecimalField(decimal_places=6, max_digits=14, default=0.00)
 
+    # Calculated based on the previous points
+    start_coordinates_lat = models.DecimalField(decimal_places=6, max_digits=14, default=0.00)
+    start_coordinates_lon = models.DecimalField(decimal_places=6, max_digits=14, default=0.00)
+
 
     # Date leaving
     date_leaving = models.DateField(auto_now=False, auto_now_add=False)
