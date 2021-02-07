8ac0bd20226af27f4f865c8010926c3d9c88e9b8
cjvidro <cjvidro@mtu.edu>
2021-02-07 09:33:02 -0500

    Disable selling

--- mainsite/selling/urls.py
+++ mainsite/selling/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.conf import settings
 from . import views
 
 app_name = 'selling'
-urlpatterns = [
-    path('', views.index, name='index')
-]
+if not settings.DISABLE_SELLING:
+	urlpatterns = [
+	    path('', views.index, name='index')
+	]
+else:
+  urlpatterns = [
+    path('', views.disabled, name="index"),
+    re_path(r'^.*/$', views.disabled, name='index'),
+  ]
