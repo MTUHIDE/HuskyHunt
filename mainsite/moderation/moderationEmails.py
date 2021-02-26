f4e0d83ad422642f61796a038a19b9039907f567
cjvidro <cjvidro@mtu.edu>
2021-02-26 11:20:13 -0500

    Initial suspend user email

--- mainsite/moderation/moderationEmails.py
+++ mainsite/moderation/moderationEmails.py
@@ -61,7 +61,38 @@ def sendRemoveRideEmail(ride, reason):
         )
     email.send();
 
-# def sendSuspendUserEmail():
+# Sends an email notification to a user who is being suspended
+# Parameters:
+#   user: The user being suspended
+#   type: The type of suspension (seven, thirty, ban)
+#   reason: A string giving the reason for suspension
+def sendSuspendUserEmail(user, type, reason):
+    #The body of the email
+    message = ('Hello ' + user.first_name + ',\n\nYour account has been ');
+
+    if (type == "seven"):
+    	message += 'suspended for seven days. '
+    elif (type == "thirty"):
+    	message += 'suspended for thirty days. '
+    else:
+    	message += 'banned. '
+
+    message += (
+        'The reason for this is "' + reason + '".' + 
+        '\n\nFor more information, contact the HuskyHunt team at huskyhunt-l@mtu.edu\n\n\nThis is an automated message.');
+
+    #The email that this message is sent from
+    from_email = 'Admin via HuskyHunt <admin@huskyhunt.com>'
+    to_email = user.email
+
+    email = EmailMessage(
+        'Account Suspension', # subject
+        message, #body
+        from_email, # from_email
+        [to_email],  # to email
+        reply_to=['huskyhunt-l@mtu.edu'],  # reply to email
+        )
+    email.send();
 
 # def sendItemMarkedAsAcceptableEmail():
 
