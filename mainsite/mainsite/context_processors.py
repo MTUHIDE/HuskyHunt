9f62499ba427b26a9ea5dc73e627468b054f051e
cjvidro <cjvidro@mtu.edu>
2021-04-05 17:26:31 -0400

    Allow covid warnings to be set to false (by default is checks whether env is defined or not instead of the value of it)

--- mainsite/mainsite/context_processors.py
+++ mainsite/mainsite/context_processors.py
@@ -7,5 +7,7 @@ def export_covid_var(request):
 
   if os.environ.get('ENABLE_COVID_WARNING') is None:
     data['ENABLE_COVID_WARNING_CLASS'] = 'covid-warning-disabled'
+  elif (os.environ.get('ENABLE_COVID_WARNING') == 'false'):
+    data['ENABLE_COVID_WARNING_CLASS'] = 'covid-warning-disabled'
 
   return data
