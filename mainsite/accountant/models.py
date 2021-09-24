b60e877a6554fc45a1dba3ac80cbef30825a2f3a
-g <cjvidro@mtu.edu>
2021-09-24 13:08:34 -0400

    Accountant state choices is restricted to actual states

--- mainsite/accountant/models.py
+++ mainsite/accountant/models.py
@@ -20,11 +20,64 @@ def validate_zip(value) :
             params={'value': value},
         )
 
+STATE_CHOICES = [
+    ('Alabama', 'Alabama'),
+    ('Alaska', 'Alaska'),
+    ('Arizona', 'Arizona'),
+    ('Arkansas', 'Arkansas'),
+    ('California', 'California'),
+    ('Colorado', 'Colorado'),
+    ('Connecticut', 'Connecticut'),
+    ('Delaware', 'Delaware'),
+    ('Florida', 'Florida'),
+    ('Georgia', 'Georgia'),
+    ('Hawaii', 'Hawaii'),
+    ('Idaho', 'Idaho'),
+    ('Illinois', 'Illinois'),
+    ('Indiana', 'Indiana'),
+    ('Iowa', 'Iowa'),
+    ('Kansas', 'Kansas'),
+    ('Kentucky', 'Kentucky'),
+    ('Louisiana', 'Louisiana'),
+    ('Maine', 'Maine'),
+    ('Maryland', 'Maryland'),
+    ('Massachusetts', 'Massachusetts'),
+    ('Michigan', 'Michigan'),
+    ('Minnesota', 'Minnesota'),
+    ('Mississippi', 'Mississippi'),
+    ('Missouri', 'Missouri'),
+    ('Montana', 'Montana'),
+    ('Nebraska', 'Nebraska'),
+    ('Nevada', 'Nevada'),
+    ('New Hampshire', 'New Hampshire'),
+    ('New Jersey', 'New Jersey'),
+    ('New Mexico', 'New Mexico'),
+    ('New York', 'New York'),
+    ('North Carolina', 'North Carolina'),
+    ('North Dakota', 'North Dakota'),
+    ('Ohio', 'Ohio'),
+    ('Oklahoma', 'Oklahoma'),
+    ('Oregon', 'Oregon'),
+    ('Pennsylvania', 'Pennsylvania'),
+    ('Rhode Island', 'Rhode Island'),
+    ('South Carolina', 'South Carolina'),
+    ('South Dakota', 'South Dakota'),
+    ('Tennessee', 'Tennessee'),
+    ('Texas', 'Texas'),
+    ('Utah', 'Utah'),
+    ('Vermont', 'Vermont'),
+    ('Virginia', 'Virginia'),
+    ('Washington', 'Washington'),
+    ('West Virginia', 'West Virginia'),
+    ('Wisconsin', 'Wisconsin'),
+    ('Wyoming', 'Wyoming')
+]
+
 class user_profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE);
     preferred_name = models.CharField(max_length=50, blank = True, null=True);
     home_city = models.CharField(max_length=50, blank = True, null=True);
-    home_state = models.CharField(max_length=50, blank = True, null=True);
+    home_state = models.CharField(max_length=50, blank = True, choices=STATE_CHOICES, null=True);
     zipcode = models.CharField(blank = True, null=True, max_length=5, validators=[validate_zip, MinLengthValidator(5)]);
     picture = models.ImageField(upload_to=upload_directory, height_field=None, width_field=None, blank = True, null=True);
     last_email = models.DateTimeField(null=True, auto_now_add=False);
