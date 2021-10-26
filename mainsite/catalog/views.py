2cfa3d810cefd6175e7caec5db11d3ec63b5b83f
Thawng Hmung <tthmung@mtu.edu>
2021-10-26 18:33:15 -0400

    Update views.py
    This lock the item, if it updated. When updated the item the item will be hidden and will appear on the moderator page for review.

--- mainsite/catalog/views.py
+++ mainsite/catalog/views.py
@@ -386,8 +386,14 @@ def update(request, pk):
         catalog_form = SellingForm(request.POST, request.FILES, instance=item, request=request)
 
         if catalog_form.is_valid():
-            catalog_form.save()
-
+            catalog_item = catalog_form.save(commit=False)
+            
+            catalog_item.archived = True
+            catalog_item.archivedType = ArchivedType.Types.HIDDEN
+            catalog_item.reported = True
+            
+            catalog_item.save()
+                        
             return HttpResponseRedirect('/catalog/' + str(pk))
         else:
             return render(request, template, {
